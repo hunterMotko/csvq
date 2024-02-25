b35ef850b03054293f672651c53e2e4298e1666c
hunterMotko <hunterMotko@gmail.com>
2024-02-24 21:09:05 -0500

    Fix: capturing stdout; Feat: cut style range

--- cmd/csv/csv.go
+++ cmd/csv/csv.go
@@ -4,29 +4,32 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type Csv struct {
 	Headers map[string]int
+	HeadLen int
 	Records [][]string
 	Lines   int
 }
 
 func NewCsv(reader *csv.Reader, headers []string) (*Csv, error) {
 	head := make(map[string]int)
+	hLen := 0
 	for i, v := range headers {
 		head[v] = i
+		hLen++
 	}
-
 	records, err := reader.ReadAll()
 	if err != nil {
 		return nil, fmt.Errorf("Error reading csv: %v\n", err)
 	}
-
 	return &Csv{
 		Headers: head,
+		HeadLen: hLen,
 		Records: records,
 		Lines:   len(records),
 	}, nil
@@ -36,8 +39,7 @@ func (c *Csv) GetColumnsByIndex(args []string) error {
 	if len(args) == 0 {
 		return fmt.Errorf("no columns specified")
 	}
-
-	writer := csv.NewWriter(os.Stdout) 
+	writer := csv.NewWriter(os.Stdout)
 	defer writer.Flush()
 	temp := make([]string, len(args))
 	for _, rec := range c.Records {
@@ -52,43 +54,69 @@ func (c *Csv) GetColumnsByIndex(args []string) error {
 			return err
 		}
 	}
-
 	return nil
 }
 
-func (c *Csv) GetColumnsBySlice(slice string) error {
+func HandleSliceString(slice string, hLen int) (int, int, error) {
 	slice = strings.Trim(slice, "[]")
-	sliceStr := strings.Split(slice, ":")
-	if len(sliceStr) != 2 {
-		return fmt.Errorf("invalid slice: %s", slice)
+	var err error
+	var start, end int
+	if len(slice) == 2 {
+		sl := []rune(slice)
+		if sl[0] == '-' {
+			start = 0
+			end = int(sl[1] - '0')
+		} else if sl[1] == '-' {
+			start = int(sl[0] - '0')
+			end = hLen
+		}
+	} else {
+		sliceStr := strings.Split(slice, "-")
+		if len(sliceStr) != 2 {
+			return 0, 0, fmt.Errorf("invalid slice: %s", slice)
+		}
+		start, err = strconv.Atoi(sliceStr[0])
+		if err != nil {
+			return 0, 0, fmt.Errorf("Invalid Atoi Conversion: %s", slice)
+		}
+		end, err = strconv.Atoi(sliceStr[1])
+		if err != nil {
+			return 0, 0, fmt.Errorf("Invalid Atoi Conversion: %s", slice)
+		}
+		if end > hLen {
+			return start, end, fmt.Errorf("Index out of bounds: %d\n Your column length is:  %d\n", end, hLen)
+		}
 	}
-	start, err := strconv.Atoi(sliceStr[0])
+	return start, end, nil
+}
+
+func (c *Csv) GetColumnsBySlice(s string) error {
+	start, end, err := HandleSliceString(s, c.HeadLen)
 	if err != nil {
-		return fmt.Errorf("Invalid Atoi Conversion: %s", slice)
+		return err
 	}
-	end, err := strconv.Atoi(sliceStr[1])
-	if err != nil {
-		return fmt.Errorf("Invalid Atoi Conversion: %s", slice)
-	}
+
 	var headers []string
 	for k := range c.Headers {
 		headers = append(headers, k)
 	}
 
+	sort.SliceStable(headers, func(i, j int) bool {
+		return c.Headers[headers[i]] < c.Headers[headers[j]]
+	})
+	
 	writer := csv.NewWriter(os.Stdout)
 	if err := writer.Write(headers[start:end]); err != nil {
 		return err
 	}
-
 	for _, rec := range c.Records {
 		if err := writer.Write(rec[start:end]); err != nil {
 			return err
 		}
 	}
 	writer.Flush()
-	// 	fmt.Fprintf(os.Stdout, "%s\n", strings.Join(headers[start:end], ","))
-	// 	for _, record := range c.Records {
-	// 		fmt.Fprintf(os.Stdout, "%s\n", strings.Join(record[start:end], ","))
-	// 	}
+	if err := writer.Error(); err != nil {
+		return fmt.Errorf("Write ERROR: %v", err)
+	}
 	return nil
 }
--- cmd/csv/csv_test.go
+++ cmd/csv/csv_test.go
@@ -1,49 +1,154 @@
 package csv_test
 
 import (
+	"bytes"
 	"encoding/csv"
+	"io"
+	"log"
+	"os"
 	"reflect"
+	"strings"
 	"testing"
 
 	Csv "github.com/hunterMotko/csvq/cmd/csv"
 )
 
-const data string = `
-"LatD", "LatM", "LatS", "NS", "LonD", "LonM", "LonS", "EW", "City", "State"
-   41,    5,   59, "N",     80,   39,    0, "W", "Youngstown", OH
-   42,   52,   48, "N",     97,   23,   23, "W", "Yankton", SD
-   46,   35,   59, "N",    120,   30,   36, "W", "Yakima", WA
-   42,   16,   12, "N",     71,   48,    0, "W", "Worcester", MA
-   43,   37,   48, "N",     89,   46,   11, "W", "Wisconsin Dells", WI
-   36,    5,   59, "N",     80,   15,    0, "W", "Winston-Salem", NC
-   49,   52,   48, "N",     97,    9,    0, "W", "Winnipeg", MB
-   39,   11,   23, "N",     78,    9,   36, "W", "Winchester", VA
-   34,   14,   24, "N",     77,   55,   11, "W", "Wilmington", NC
-   39,   45,    0, "N",     75,   33,    0, "W", "Wilmington", DE
-   48,    9,    0, "N",    103,   37,   12, "W", "Williston", ND
-   41,   15,    0, "N",     77,    0,    0, "W", "Williamsport", PA
-   37,   40,   48, "N",     82,   16,   47, "W", "Williamson", WV
-   33,   54,    0, "N",     98,   29,   23, "W", "Wichita Falls", TX
-   37,   41,   23, "N",     97,   20,   23, "W", "Wichita", KS
-   40,    4,   11, "N",     80,   43,   12, "W", "Wheeling", WV
-   26,   43,   11, "N",     80,    3,    0, "W", "West Palm Beach", FL
-   47,   25,   11, "N",    120,   19,   11, "W", "Wenatchee", WA
-   41,   25,   11, "N",    122,   23,   23, "W", "Weed", CA
-`
-
-func initalizeFile() *Csv.Csv {
-	var reader *csv.Reader
-	reader = csv.NewReader()
-	return Csv.NewCsv(reader)
+var data []string = []string{
+	"LatD,LatM,LatS,NS,LonD,LonM,LonS,EW,City,State",
+	"41,5,59,N,80,39,0,W,Youngstown,OH",
+	"42,52,48,N,97,23,23,W,Yankton,SD",
+	"46,35,59,N,120,30,36,W,Yakima,WA",
+	"42,16,12,N,71,48,0,W,Worcester,MA",
+	"43,37,48,N,89,46,11,W,Wisconsin Dells,WI",
+	"36,5,59,N,80,15,0,W,Winston-Salem,NC",
+	"49,52,48,N,97,9,0,W,Winnipeg,MB",
+	"39,11,23,N,78,9,36,W,Winchester,VA",
+	"34,14,24,N,77,55,11,W,Wilmington,NC",
+	"39,45,0,N,75,33,0,W,Wilmington,DE",
+	"48,9,0,N,103,37,12,W,Williston,ND",
+	"41,15,0,N,77,0,0,W,Williamsport,PA",
+	"37,40,48,N,82,16,47,W,Williamson,WV",
+	"33,54,0,N,98,29,23,W,Wichita Falls,TX",
+	"37,41,23,N,97,20,23,W,Wichita,KS",
+	"40,4,11,N,80,43,12,W,Wheeling,WV",
+	"26,43,11,N,80,3,0,W,West Palm Beach,FL",
+	"47,25,11,N,120,19,11,W,Wenatchee,WA",
+	"41,25,11,N,122,23,23,W,Weed,CA",
 }
 
-func TestCsv(t *testing.T) {
-	cv := initalizeFile()
-	t.Run("test files on creating new csv struct", func(t *testing.T) {
-		headers := cv.GetHeaders()
-		expected := []string{"LatD", "LatM", "LatS", "NS", "LonD", "LonM", "LonS", "EW", "City", "State"}
-		if !reflect.DeepEqual(headers, expected) {
-			t.Errorf("wanted headers %v, to be a matching array as expected %v\n", headers, expected)
+var expHeaders map[string]int = map[string]int{
+	"LatD": 0, "LatM": 1, "LatS": 2, "NS": 3, "LonD": 4, "LonM": 5, "LonS": 6, "EW": 7, "City": 8, "State": 9,
+}
+
+func TestCsvInit(t *testing.T) {
+	cv := initCsv()
+	t.Run("Check inital headers on NewCsv", func(t *testing.T) {
+		for k, v := range cv.Headers {
+			val, ok := expHeaders[k]
+			if !ok {
+				t.Errorf("Not in map! %s, %v", k, ok)
+			}
+			if val != v {
+				t.Errorf("Val %d, not equal to %d", val, v)
+			}
 		}
 	})
+	t.Run("Check inital columns on NewCsv", func(t *testing.T) {
+		exLen := 19
+		exCols := data[1:]
+		if cv.Lines != exLen {
+			t.Errorf("lines %v, should equal %v\n", cv.Lines, exLen)
+		}
+		for i, rec := range cv.Records {
+			col := strings.Split(exCols[i], ",")
+			if !reflect.DeepEqual(rec, col) {
+				t.Errorf("column %v,\n should equal %v\n", rec, col)
+			}
+		}
+	})
+}
+
+func TestSlices(t *testing.T) {
+	cv := initCsv()
+	testCases := []struct {
+		desc     string
+		sliceStr string
+		expect   string
+	}{
+		{
+			desc:     "common slice",
+			sliceStr: "[1-4]",
+			expect:   MockCSV(SliceColumns(1, 4)),
+		},
+		{
+			desc:     "start col to end of columns slice",
+			sliceStr: "[5-]",
+			expect:   MockCSV(SliceColumns(5, cv.HeadLen)),
+		},
+		{
+			desc:     "begining to N column",
+			sliceStr: "[-5]",
+			expect:   MockCSV(SliceColumns(0, 5)),
+		},
+		// {
+		// 	desc:     "out of bounds error",
+		// 	sliceStr: "[5-20]",
+		// 	expect:   `slice bounds out of range [:19] with capacity 10`,
+		// },
+	}
+	for _, cur := range testCases {
+		t.Run(cur.desc, func(t *testing.T) {
+			got := captureStdout(func() {
+				cv.GetColumnsBySlice(cur.sliceStr)
+			})
+			if got != cur.expect {
+				t.Errorf("got %s, but expected %s\n", got, cur.expect)
+			}
+		})
+	}
+}
+
+func initCsv() *Csv.Csv {
+	buf := bytes.NewBuffer([]byte(strings.Join(data, "\n")))
+	reader := csv.NewReader(buf)
+	head, _ := reader.Read()
+	cv, err := Csv.NewCsv(reader, head)
+	if err != nil {
+		log.Fatal("something didnt work on initalize")
+	}
+	return cv
+}
+
+func MockCSV(m [][]string) string {
+	var buf bytes.Buffer
+	wrt := csv.NewWriter(&buf)
+	wrt.WriteAll(m)
+	return buf.String()
+}
+
+func SliceColumns(x, y int) [][]string {
+	var res [][]string
+	for _, row := range data {
+		cols := strings.Split(row, ",")
+		res = append(res, cols[x:y])
+	}
+	return res
+}
+
+func captureStdout(f func()) string {
+	defer func(org *os.File) {
+		os.Stdout = org
+	}(os.Stdout)
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	os.Stdout = w
+	f()
+	w.Close()
+	out, err := io.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
 }
